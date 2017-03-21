65c22978abe1bed2c5fd7d2135dd05b641f09d20
Muhammad Qumail <mqumail@brandonassociates.com>
2017-03-21 12:56:17 -0400

    First commit

---
+++ app/src/main/java/com/example/mqumail/helloimage/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mqumail.helloimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView a = (ImageView) findViewById(R.id.imageView);
+        Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(a);
+    }
+}
